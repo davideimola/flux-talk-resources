9da9940519a6bd17ddb3eed6a1a8d48c0067767b
Davide Imola <davide.imola@redcarbon.ai>
2023-02-13 10:24:19 +0100

    feat: adding resources

---
+++ 1_flux_bootstrap.sh
@@ -0,0 +1,8 @@
+flux bootstrap github \
+	--owner=davideimola \
+	--repository=di-cluster-fleet \
+	--path=clusters/carbonello \
+	--components-extra=image-reflector-controller,image-automation-controller \
+	--branch=main \
+	--personal \
+	--read-write-key
